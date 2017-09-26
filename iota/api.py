c1b4d3c1c47262748b646cde29d2aec3bc26ddcb
Brandon Moller <brandon@bkmoller.net>
2017-09-25 22:10:55 -0600

    Make the things more Pythonic

--- iota/api.py
+++ iota/api.py
@@ -45,21 +45,37 @@ class BMWiApiClient(object):
     access_token = str
     api_key = str
     api_secret = str
+    refresh_token = str
     region = str
-    user_email = str
-    user_password = str
 
-    _API_ENDPOINT = '/webapi/v1/user/'
-    _API_SERVER_CHINA = 'https://b2vapi.bmwgroup.cn:8592'
-    _API_SERVER_EUROPE = 'https://b2vapi.bmwgroup.com'
-    _API_SERVER_US = 'https://b2vapi.bmwgroup.us'
-    _AUTHENTICATION_ENDPOINT = '/webapi/oauth/token/'
+    API_ENDPOINT = '/webapi/v1/user/'
+    API_SERVER_CHINA = 'https://b2vapi.bmwgroup.cn:8592'
+    API_SERVER_EUROPE = 'https://b2vapi.bmwgroup.com'
+    API_SERVER_US = 'https://b2vapi.bmwgroup.us'
+    AUTHENTICATION_ENDPOINT = '/webapi/oauth/token/'
 
     def __init__(
         self, api_key: str, api_secret: str, username: str=None,
-        password: str=None, refresh_token: str=None, region: str='US'
+        password: str=None, refresh_token: str=None, region: str='US',
+        access_token: str=None
     ):
         """Create a new instance.
+
+        There are several different valid combinations of authentication
+        information:
+
+            - A ConnectedDrive username and password. These are provided to the
+              API to retrieve an OAuth access token and an OAuth refresh token.
+            - An OAuth refresh token. This requires that the token was stored
+              from a previous authentication process. This token is used to
+              retrieve new access and refresh tokens; the new refresh token
+              should then be stored.
+            - An OAuth access token and an OAuth refresh token. The access
+              token will be used until it expires, after which a new pair of
+              access and refresh tokens will be retrieved.
+
+        Any provided username and password will not be stored in the object;
+        they are used to retrieve tokens and then left for garbage collection.
 
         Args:
             api_key: API key for the ConnectedDrive service.
@@ -72,6 +88,9 @@ class BMWiApiClient(object):
                 provided a username and password are not required; a refresh
                 token is the preferred authentication method.
             region: A region to connect to for the ConnectedDrive service.
+            access_token: To support caching of credentials across short-lived
+                processes, an access token can optionally be provided that
+                will be used to authenticate.
 
         Raises:
             RuntimeError: Occurs when the constructor does not receive required
@@ -81,7 +100,9 @@ class BMWiApiClient(object):
         """
 
         if (
-            (username is None or password is None) and refresh_token is None
+            (username is None or password is None)
+            and refresh_token is None
+            and (access_token is None or refresh_token is None)
         ):
             raise RuntimeError(
                 'Either refresh_token or both user_email and user_password '
@@ -90,16 +111,22 @@ class BMWiApiClient(object):
 
         self.api_key = api_key
         self.api_secret = api_secret
-        self._vehicles = {}
+        self.refresh_token = refresh_token
+        self.region = region
+        self.access_token = access_token
 
         try:
-            self._API_URL = urllib.parse.urljoin(
-                getattr(self, '_API_SERVER_{region}'.format(region=region)),
-                self._API_ENDPOINT
+            self.API_URL = urllib.parse.urljoin(
+                getattr(
+                    self, 'API_SERVER_{region}'.format(region=self.region)
+                ),
+                self.API_ENDPOINT
             )
-            self._AUTHENTICATION_URL = urllib.parse.urljoin(
-                getattr(self, '_API_SERVER_{region}'.format(region=region)),
-                self._AUTHENTICATION_ENDPOINT
+            self.AUTHENTICATION_URL = urllib.parse.urljoin(
+                getattr(
+                    self, 'API_SERVER_{region}'.format(region=self.region)
+                ),
+                self.AUTHENTICATION_ENDPOINT
             )
         except KeyError:
             raise KeyError(
@@ -110,20 +137,22 @@ class BMWiApiClient(object):
                 )
             )
 
-        self.access_token, self.refresh_token = self.get_access_token(
-            username=username, password=password, refresh_token=refresh_token)
+        if self.access_token is None:
+            self.access_token, self.refresh_token = self.get_access_token(
+                username=username, password=password
+            )
 
     def get_access_token(
-        self, username: str=None, password: str=None, refresh_token: str=None
+        self, username: str=None, password: str=None
     ) -> tuple:
-        """Authenticate against the BMW i API and get a new OAuth token.
+        """Authenticate against the BMW API and get new OAuth tokens.
+
+        If no username and password are supplied this instance's internal
+        refresh token will be used to retrieve new tokens.
 
         Args:
             username: A ConnectedDrive user account e-mail address.
             password: The password for the ConnectedDrive account.
-            refresh_token: A OAuth refresh token returned by a previous
-                authentication request. If this parameter is supplied any
-                username and password also supplied are ignored.
 
         Returns:
             A tuple with both a new OAuth authorization token and a new OAuth
@@ -136,33 +165,32 @@ class BMWiApiClient(object):
             InvalidCredentialsException: Authentication against the API failed.
         """
 
-        authentication_string = '{key}:{secret}'.format(
+        authorization_string = '{key}:{secret}'.format(
             key=self.api_key, secret=self.api_secret
         )
-        authentication_string_bytes = authentication_string.encode()
-        base64_auth = base64.b64encode(authentication_string_bytes)
-        base64_auth_token = str(base64_auth, encoding='utf8')
+        base64_authorization = base64.b64encode(authorization_string.encode())
+        authorization_token = str(base64_authorization, encoding='utf8')
         form_data = {
             'scope': 'remote_services vehicle_data',
         }
-        if refresh_token:
-            form_data.update({
-                'grant_type': 'refresh_token',
-                'refresh_token': refresh_token,
-            })
-        else:
+        if username and password:
             form_data.update({
                 'grant_type': 'password',
                 'password': password,
                 'username': username,
             })
+        else:
+            form_data.update({
+                'grant_type': 'refresh_token',
+                'refresh_token': self.refresh_token,
+            })
         post_data = urllib.parse.urlencode(form_data).encode()
         request_headers = {
-            'Authorization': 'Basic {token}'.format(token=base64_auth_token),
+            'Authorization': 'Basic {token}'.format(token=authorization_token),
             'Content-Type': 'application/x-www-form-urlencoded',
         }
         authentication_request = urllib.request.Request(
-            self._AUTHENTICATION_URL, data=post_data, headers=request_headers)
+            self.AUTHENTICATION_URL, data=post_data, headers=request_headers)
 
         try:
             with urllib.request.urlopen(authentication_request) as http_response:
@@ -197,9 +225,9 @@ class BMWiApiClient(object):
             data: Bytes to POST in the API call.
             method: The HTTP method to use for the request (eg GET or POST).
             parse_as_json: Most API methods return a text response in JSON
-                           format; this method can parse those to Python
-                           objects. However, a few return binary data and this
-                           parameter enables access to the raw bytes.
+                format; this method can parse those to Python objects. However,
+                a few return binary data and this parameter enables access to
+                the raw bytes.
 
         Returns:
             The response from the BMW API. If `parse_as_json` is `True` this
@@ -222,7 +250,7 @@ class BMWiApiClient(object):
             'Content-Type': 'application/x-www-form-urlencode',
         }
         api_request = urllib.request.Request(
-            urllib.parse.urljoin(self._API_URL, api_endpoint), data=data,
+            urllib.parse.urljoin(self.API_URL, api_endpoint), data=data,
             headers=request_headers, method=method)
 
         try:
@@ -236,38 +264,27 @@ class BMWiApiClient(object):
 
         return api_response
 
-    def _get_vehicles(self):
-        """Refresh the cached vehicle list.
-        """
-
-        self._vehicles = {}
-        api_response = self.call_endpoint('vehicles')
-        for vehicle_record in api_response['vehicles']:
-            self._vehicles[vehicle_record['vin']] = vehicle_record
-
-    def vehicles(self, refresh: bool=False) -> list:
+    def list_vehicles(self) -> list:
         """Get a list of all vehicles registered with the current account.
-
-        Args:
-            refresh: Discard any cached data and make an API request for the
-                     current list.
 
         Returns:
             List of VIN's of the registered vehicles.
         """
 
-        if not self._vehicles or refresh:
-            self._get_vehicles()
+        vehicles = []
+        api_response = self.call_endpoint('vehicles')
+        for vehicle_record in api_response['vehicles']:
+            vehicles.append(vehicle_record['vin'])
 
-        return list(self._vehicles.keys())
+        return vehicles
 
     def get_vehicle(self, vin: str) -> vehicle.Vehicle:
         """Retrieve a specific vehicle by its VIN.
 
         Args:
             vin: A VIN to query and return as a Vehicle object. In order to
-                 succeed the corresponding vehicle must be registered in the
-                 ConnectedDrive portal.
+                succeed the corresponding vehicle must be registered in the
+                ConnectedDrive portal.
 
         Returns:
             A new Vehicle representing the requested vehicle.
@@ -276,17 +293,19 @@ class BMWiApiClient(object):
             KeyError: No vehicle with a matching VIN is listed in this account.
         """
 
-        # Do the logical thing and try refreshing the list once before failing
-        if not self._vehicles or vin not in self._vehicles:
-            self._get_vehicles()
-        if vin not in self._vehicles:
+        vehicle_data = None
+        api_response = self.call_endpoint('vehicles')
+        for vehicle_record in api_response['vehicles']:
+            if vehicle_record['vin'] == vin:
+                vehicle_data = vehicle_record
+                break
+        else:
             raise KeyError(
                 'No vehicle with VIN {vin} is registered'.format(vin=vin)
             )
-
         api_response = self.call_endpoint(
             'vehicles/{vin}/status'.format(vin=vin))
 
         return vehicle.Vehicle(
-            self, self._vehicles[vin], api_response['vehicleStatus']
+            self, vehicle_data, api_response['vehicleStatus']
         )
